91033eba9558867cadad646be0a45ef0c6316b46
AparCode <apar2003@gmail.com>
2024-11-02 19:44:22 -0400

    tested API framework on Python

--- main.py
+++ main.py
@@ -2,5 +2,22 @@
 # @authors: Tahiba Rahman, Aparnaa Senthilnathan, Stacey Zheng
 # A Hack.COMS 2024 Hackathon Project
 
-import mysql.connector
+from flask import Flask, jsonify, request
 
+  
+he = {'test':'testing', 'test2':'testing22'}
+app = Flask(__name__) 
+  
+@app.route('/', methods = ['GET']) 
+def home(): 
+    if(request.method == 'GET'): 
+        return jsonify(he) 
+  
+  
+@app.route('/home/<int:num>', methods = ['GET']) 
+def disp(num): 
+    return jsonify({'data': num**2}) 
+  
+  
+if __name__ == '__main__': 
+    app.run(debug = True) 
